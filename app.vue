433e53eb2c59a1ba088b15baed081958b6b127ca
hsm0711 <bingo8@nate.com>
2023-11-22 00:41:17 +0900

    api 서버 도메인 변경

--- app.vue
+++ app.vue
@@ -9,6 +9,7 @@
 <script setup>
 import { ofetch } from 'ofetch'
 
-globalThis.$fetch = ofetch.create({ baseURL: 'https://bingo-playground.duckdns.org:8080' })
+//globalThis.$fetch = ofetch.create({ baseURL: 'https://bingo-playground.duckdns.org:8080' })
 //globalThis.$fetch = ofetch.create({ baseURL: 'http://localhost:8080' })
+globalThis.$fetch = ofetch.create({ baseURL: 'https://august-cascade-385602.uw.r.appspot.com' })
 </script>
