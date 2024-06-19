c59224b64e5068aabe3f36d60917962866291d38
parkjunwon <junwon9978@naver.com>
2024-06-19 13:42:08 +0900

    PLAYG-118-투표기능수정Front

--- stores/useAuthStore.ts
+++ stores/useAuthStore.ts
@@ -1,24 +1,29 @@
-import { defineStore } from "pinia"
+import { defineStore } from 'pinia'
 import { useRouter } from 'vue-router'
+import { useCookie } from 'nuxt/app'
+import { computed, ref } from 'vue'
 
-export const useAuthStore = defineStore('auth', () => {
+export const useAuthStore = defineStore(
+  'auth',
+  () => {
+    const router = useRouter()
 
-  const router = useRouter()
+    const mberId = ref('')
+    const token = ref('')
 
-  const mberId = ref('')
-  const token = ref('')
+    const isLogin = computed(() => (token.value == '' ? false : true))
 
-  const isLogin = computed(() => token.value == ''? false : true)
+    function logout() {
+      useCookie('token', { expires: new Date(), watch: false })
+      mberId.value = ''
+      token.value = ''
+      alert('로그아웃되었습니다.')
+      return router.push('/login')
+    }
 
-  function logout() {
-    mberId.value = ''
-    token.value = ''
-    alert("로그아웃되었습니다.")
-    return router.push('/login')
+    return { mberId, token, isLogin, logout }
+  },
+  {
+    persist: true
   }
-
-  return { mberId, token, isLogin, logout }
-}, {
-  persist: true
-})
-
+)
