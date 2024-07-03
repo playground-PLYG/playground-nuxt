d7dfc32ee1d30537a7f0be86d3e084a750cf8e47
hsm0711 <bingo8@nate.com>
2024-07-03 10:59:26 +0900

    input slot추가

--- components/DkInput.vue
+++ components/DkInput.vue
@@ -1,3 +1,15 @@
 <template>
-  <q-input v-bind="$attrs" outlined round dense flat class="dk-input" />
+  <q-input v-bind="attrs" outlined round dense flat class="dk-input">
+    <template v-for="(_, name) in slots" :key="name">
+      <slot :name="name" v-bind="slots[name]" />
+    </template>
+  </q-input>
 </template>
+
+<script setup lang="ts">
+import type { QInputProps, QInputSlots } from 'quasar'
+import { useAttrs, useSlots } from 'vue'
+
+const slots = useSlots() as unknown as QInputSlots
+const attrs = useAttrs() as unknown as QInputProps
+</script>
