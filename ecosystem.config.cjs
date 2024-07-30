9360b9af1ee0be0180c7dfbc6d99c722d7ea3444
hsm0711 <bingo8@nate.com>
2024-07-30 18:18:32 +0900

    pinpoint 적용

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,13 +7,13 @@ module.exports = {
       instances: '2',
       autostart: true,
       script: './.output/server/index.mjs',
+      node_args: ['-r', 'pinpoint-node-agent'],
       env: {
         HOST: '0.0.0.0',
         PORT: 3000,
         NODE_ENV: 'production',
-        PINPOINT_APPLICATION_NAME: 'playground',
-        PINPOINT_AGENT_ID: 'web',
-        PINPOINT_COLLECTOR_IP: 'localhost'
+        PINPOINT_APPLICATION_NAME: 'playground-web',
+        PINPOINT_AGENT_ID: 'nuxt'
       }
     }
   ]
