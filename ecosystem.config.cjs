ee7e0977e3afd7dd9fd543fb30bbe9bb11329d6f
hsm0711 <bingo8@nate.com>
2024-08-06 11:04:35 +0900

    pm2 설정 수정

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,13 +6,14 @@ module.exports = {
       exec_mode: 'cluster',
       instances: '2',
       autostart: true,
+      watch: true,
       script: './.output/server/index.mjs',
       node_args: ['-r', 'pinpoint-node-agent'],
       env: {
         HOST: '0.0.0.0',
         PORT: 3000,
         NODE_ENV: 'production',
-        ENV_TEXT: '${NODE_APP_INSTANCE}',
+        PINPOINT_ENABLE: true,
         PINPOINT_AGENT_ID: 'nuxt',
         PINPOINT_APPLICATION_NAME: 'playground-web'
       }
