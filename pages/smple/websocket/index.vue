9a573f1849586b1d149fbe8b3fdee847c2ae9cef
hsm0711 <bingo8@nate.com>
2024-08-09 11:51:08 +0900

    websocket 테스트 페이지 추가필드 속성 추가

--- pages/smple/websocket/index.vue
+++ pages/smple/websocket/index.vue
@@ -11,29 +11,101 @@
         <div class="text-h6">테스트</div>
       </div>
 
-      <div class="q-pa-md">
-        <div class="row">
-          <div class="col-5">
-            <dk-select
-              v-model="messageType"
-              :options="messageTypeOption"
-              label="messageType"
-            />
-          </div>
-          <div class="col-5">
-            <dk-input v-model="senderId" label="senderId" />
-          </div>
-        </div>
-        <div class="row q-mt-xs">
-          <div class="col-8">
-            <dk-input
-              v-model="message"
-              label="메시지내용"
-              clearable
-              autofocus
-            />
-          </div>
-          <div class="col-2 q-ml-lg">
+      <div class="q-pa-md q-gutter-sm">
+        <q-markup-table flat bordered>
+          <colgroup>
+            <col style="width: 100px" />
+            <col />
+          </colgroup>
+          <tbody>
+            <tr>
+              <th scope="row">메시지 유형</th>
+              <td>
+                <dk-select
+                  v-model="messageType"
+                  :options="messageTypeOption"
+                  label="messageType"
+                />
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">송신자ID</th>
+              <td>
+                <dk-input v-model="senderId" label="senderId" />
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">메시지 내용</th>
+              <td>
+                <dk-input
+                  v-model="message"
+                  label="메시지내용"
+                  clearable
+                  autofocus
+                />
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">추가정보</th>
+              <td>
+                <div v-if="etcList.length == 0" class="row wrapEtcBtn">
+                  <dk-btn
+                    outline
+                    icon="add"
+                    padding="xs lg"
+                    size="sm"
+                    @click="fn_addEtc"
+                  />
+                </div>
+                <div
+                  v-for="(etcObj, idx) in etcList"
+                  v-else
+                  :key="idx"
+                  class="row wrapEtcItem q-gutter-sm"
+                >
+                  <div class="col-4">
+                    <dk-input
+                      v-model="etcObj.key"
+                      outlined
+                      label="key"
+                      class="q-mb-sm"
+                      autofocus
+                    />
+                  </div>
+                  <div class="col-4 q-ml-xs">
+                    <dk-input
+                      v-model="etcObj.val"
+                      outlined
+                      label="value"
+                      class="q-mb-sm"
+                    />
+                  </div>
+
+                  <div class="col-2 q-ml-xs q-gutter-sm">
+                    <dk-btn
+                      outline
+                      icon="remove"
+                      padding="xs lg"
+                      size="sm"
+                      @click="fn_removeEtc(idx)"
+                    />
+                    <dk-btn
+                      v-if="idx == etcList.length - 1"
+                      outline
+                      icon="add"
+                      padding="xs lg"
+                      size="sm"
+                      @click="fn_addEtc"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </q-markup-table>
+
+        <div class="row justify-end q-mt-md">
+          <div class="col-1 justify-end">
             <dk-btn label="전송" @click="onClickSend" />
           </div>
         </div>
@@ -67,6 +139,7 @@ const messageTypeOption = ref<string[]>(['TYPE_01', 'TYPE_02', 'HTML'])
 const senderId = ref<string>(authStore.mberId || 'test')
 const message = ref<string>('')
 const txtarea = ref<string>('')
+const etcList = ref<Array<{ key: string; val: any }>>([])
 
 const onClickSend = async () => {
   if (message.value == '') {
@@ -74,6 +147,11 @@ const onClickSend = async () => {
       message: '메시지를 입력해주세요'
     })
   } else {
+    const etcObj: { [key: string]: any } = {}
+    if (etcList.value.length > 0) {
+      etcList.value.forEach((obj) => (etcObj[obj.key] = obj.val))
+    }
+
     loading.show()
 
     await $fetch<ApiPagingResponse<null>>(
@@ -83,7 +161,8 @@ const onClickSend = async () => {
         body: JSON.stringify({
           messageType: messageType.value,
           senderId: senderId.value,
-          message: message.value
+          message: message.value,
+          etc: etcObj
         })
       }
     )
@@ -117,6 +196,14 @@ onMounted(() => {
     }
   )
 })
+
+const fn_addEtc = () => {
+  etcList.value.push({ key: '', val: null })
+}
+
+const fn_removeEtc = (idx: number) => {
+  etcList.value.splice(idx, 1)
+}
 </script>
 
 <style lang="scss" scoped>
